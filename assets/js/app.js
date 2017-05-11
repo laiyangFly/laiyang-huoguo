6de507de6f3b5c04d938ed26c3393db97e95a107
laiyangFly <16238099@qq.com>
2017-05-12 02:08:48 +0800

    火锅
    1.0

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Laiyang on 2017/5/11.
+ */
+$(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        autoplay: 3000,//可选选项，自动滑动
+        effect : 'fade',
+        pagination : '.swiper-pagination',
+        paginationClickable :true,
+    })
+})
