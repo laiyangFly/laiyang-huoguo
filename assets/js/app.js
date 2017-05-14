06e466b57dc4a665432109c6daaf68f26808c95a
laiyangFly <16238099@qq.com>
2017-05-15 02:07:46 +0800

    頁面
    2.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,201 @@
  * Created by Laiyang on 2017/5/11.
  */
 $(function () {
-    var mySwiper = new Swiper('.swiper-container', {
+    var mySwiper1 = new Swiper('.myswiper1', {
         autoplay: 3000,//可选选项，自动滑动
         effect : 'fade',
+        fade: {
+            crossFade: true
+        },
         pagination : '.swiper-pagination',
-        paginationClickable :true,
-    })
-})
+        paginationClickable :true
+    });
+    var mySwiper2 = new Swiper('.myswiper2', {
+        loop:true,
+        effect : 'fade',
+        fade: {
+            crossFade: true,
+        },
+        nextButton: '.arrow2',
+        prevButton: '.arrow1'
+    });
+    var swiperEat = new Swiper('.swiper-eat', {
+        loop:true,
+        slidesPerView : 4,
+        nextButton: '.arrow2',
+        prevButton: '.arrow1'
+    });
+    var swiperEat = new Swiper('.swiper-shop', {
+        loop:true,
+        slidesPerView : 1,
+        nextButton: '.arrow2',
+        prevButton: '.arrow1'
+    });
+});
+
+//留言================================
+function check(n){
+    var info = "";
+    if(n==1){//留言
+        document.myform.KS_Url.value=getCookie("Url");
+        //document.myform.KS_IP.value=getCookie("comeIp");
+        document.myform.KS_Keyword.value=getCookie("KeyWord");
+        var nameStr = document.myform.KS_Name.value;
+        var TelStr = document.myform.KS_Tel.value;
+        //var cityStr = document.myform.KS_City.value;
+        var contentStr = document.myform.KS_Content.value;
+        if (!TelStr.match(/^1[3|4|5|6|7|8|9][0-9]\d{4,8}$/) || TelStr.length!=11) {
+            info = "请输入正确的电话号码！以便及时联系您！";
+            document.getElementById("info").innerHTML = info;
+            return false;
+        }
+        if (nameStr == "") {
+            info = "请输入您的名字！";
+            document.getElementById("info").innerHTML = info;
+            return false;
+        }
+        if (contentStr == "") {
+            info = "请输入您想咨询的问题！";
+            document.getElementById("info").innerHTML = info;
+            return false;
+        }
+        if (info == "") {
+            $.post("/plus/form/form.asp", $("#myform").serialize(),function(){total()});//表单提交
+            alert("留言成功，我们将尽快与您联系！");
+            document.myform.KS_Name.value="";
+            document.myform.KS_Tel.value="";
+            //document.myform.KS_City.value="";
+            document.myform.KS_Content.value="";
+            setCookie("isJm", "y");//看加盟费
+        }
+    }else if(n==2){//加盟费
+        document.myform.KS_Url.value=getCookie("Url");
+        //document.myform.KS_IP.value=getCookie("comeIp");
+        document.myform.KS_Keyword.value=getCookie("KeyWord");
+        //document.myform.KS_Content.value="【加盟费】";
+        var TelStr = document.myform.KS_Tel.value;
+        var cityStr = document.myform.KS_City.value;
+        var nameStr = document.myform.KS_Name.value;
+        if (cityStr == "") {
+            info = "请输入您准备加盟的城市！";
+            document.getElementById("info").innerHTML = info;
+            return false;
+        }
+        if (!TelStr.match(/^1[3|4|5|6|7|8|9][0-9]\d{4,8}$/) || TelStr.length!=11) {
+            info = "请输入正确的电话号码！";
+            document.getElementById("info").innerHTML = info;
+            return false;
+        }
+        if (nameStr == "") {
+            info = "请输入您的名字！";
+            document.getElementById("info").innerHTML = info;
+            return false;
+        }
+        if (info == "") {
+            $.post("/plus/form/form.asp", $("#myform").serialize(),function(){total();});//表单提交
+            $(".jmfTable").show();
+            setCookie("isJm", "y");//看加盟费
+            document.myform.KS_Name.value="";
+            document.myform.KS_Tel.value="";
+            document.myform.KS_City.value="";
+        }
+    }else if(n==3){//预算
+        document.myform.KS_Url.value=getCookie("Url");
+        //document.myform.KS_IP.value=getCookie("comeIp");
+        document.myform.KS_Keyword.value=getCookie("KeyWord");
+        //document.myform.KS_Content.value="【预算】";
+        var CityStr = document.myform.KS_City.value;
+        var AreaStr = document.myform.KS_Area.value;
+        var TelStr = document.myform.KS_Tel.value;
+        var nameStr = document.myform.KS_Name.value;
+        if (CityStr == "") {
+            info = "请输入准备加盟的城市！";
+            document.getElementById("info").innerHTML = info;
+            return false;
+        }
+        if (isNaN(AreaStr) || AreaStr < 190) {
+            info = "请输入200平米以上的店铺面积！";
+            document.getElementById("info").innerHTML = info;
+            return false;
+        }
+        if (!TelStr.match(/^1[3|4|5|6|7|8|9][0-9]\d{4,8}$/) || TelStr.length!=11) {
+            info = "请输入正确的电话号码！";
+            document.getElementById("info").innerHTML = info;
+            return false;
+        }
+        if (nameStr == "") {
+            info = "请输入您的名字！";
+            document.getElementById("info").innerHTML = info;
+            return false;
+        }
+        if (info == "") {
+            $.post("/plus/form/form.asp", $("#myform").serialize(),function(){total()});//表单提交
+            setCookie("isJm", "y");//看加盟费
+            setCookie("City", CityStr);//计算用
+            setCookie("Area", AreaStr);//计算用
+            document.myform.KS_Name.value="";
+            document.myform.KS_Tel.value="";
+            document.myform.KS_City.value="";
+            document.myform.KS_Area.value="";
+            document.getElementById("info").innerHTML = "";
+        }
+    }
+}
+//设置来源URL
+if(getCookie("Url")==null){
+    var ref = "";//来源网址
+    if (document.referrer.length > 0) {
+        ref = document.referrer;
+    }else if(opener.location.href.length > 0) {
+        ref = opener.location.href;
+    }
+    var domain=getDomain(ref);//域名
+    setCookie("Url", domain);//写入Cookies
+    //找关键词
+    var st=0;//开始位置
+    if(ref.indexOf('query=',1)>0){//sogou
+        st=ref.indexOf('query=',1)+6;
+    }else if(ref.indexOf('search=',1)>0){//gougou
+        st=ref.indexOf('search=',1)+7;
+    }else if(ref.indexOf('w=',1)>0){//soso
+        st=ref.indexOf('w=',1)+2;
+    }else if(ref.indexOf('q=',1)>0){//google、yodao、bing、yahoo、360
+        st=ref.indexOf('q=',1)+2;
+    }else if(ref.indexOf('word=',1)>0){//百度手机
+        st=ref.indexOf('word=',1)+5;
+    }else if(ref.indexOf('keyword=',1)>0){//m.sogou.com
+        st=ref.indexOf('keyword=',1)+8;
+    }else{
+        st=0;
+    }
+    if(st>0){
+        setCookie("KeyWord", getKeyWord(ref,st));//写入Cookies
+    }
+}//设置来源URL
+//获取关键词
+function getKeyWord(url,st){
+    var en =  url.indexOf('&', st);
+    var keyWord = url.substring(st,en);
+    return decodeURI(keyWord);
+}
+//获取域名
+function getDomain(url){
+    var st = url.indexOf('//', 1);
+    var en =  url.indexOf('/', st + 2);
+    var domain = url.substring(st + 2,en);
+    return domain;
+}
+//JS操作cookies方法!
+//写cookies
+function setCookie(name, value) {
+    var Days = 30;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+//读cookies
+function getCookie(name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg)) return unescape(arr[2]);
+    else return null;
+}
---
+++ assets/js/yuSuan.js
@@ -0,0 +1,229 @@
+﻿//单价
+//var _rent = 83;//默认每平租金++++++++++++++++++++++++++++++++++++++++++
+//var _rentT = 12;//按压一付3算++++++++++++++++++++++++++++++++++++++++++
+var _zx = 1200;//默认每平装修费=====
+var _design = 80;//400平以上每平设计费=====
+var _rj = 0.28;//容积率（每平米可容纳的人数）=====
+var _ys = 5;//每桌椅子数=====
+var _zz = 2600;//餐桌/菜架/餐柜单价=====
+var _yz = 275;//每张椅子单价=====
+var _gz = 700;//锅/锅圈/电磁炉 每套单价=====
+var _bh = 1500;//每桌调味品备货费(火锅油、火锅底料、香油、鸡精)=====
+var _bc = 1000;//每桌菜品备货费=====
+var _ss = 2500;//每套员工宿舍租金=====
+//var _fw = 2500;//每月服务费
+var _cj = 150;//厨具每平米费用=====
+var _caj = 70;//餐具每平米费用=====
+var _zaj = 40;//杂件每平米费用=====
+//计算后的值
+var ggVal = 30000;//广告费(前3个月全方位营销)=====
+var bzVal = 50000;//保证金=====
+var xtVal = 14000;//营销系统费(1.2系统费+2000服务费)=====
+var pxVal = 50000;//前期人员招聘及培训=====
+var fwVal = 2500;//月服务费=====
+var area;//面积
+var city;//面积
+var rentVal;//租金
+var zxVal;//装修费
+var sDesign;//设计费
+var czVal;//餐桌椅费
+var gzVal;//锅、锅圈、电磁炉
+var cjVal;//厨具费=面积*每平米费用
+var cajVal;//餐具
+var zjVal;//杂件
+var bhVal;//调味品备货
+var bcVal;//菜品备货
+var ssVal;//员工宿舍
+var totalVal;//总投资
+var zsVal;//桌数
+var cwVal;//餐位数
+
+var jmf1=250000;//北京、上海、广州、深圳、天津=====
+var jmf2=200000;//二三线城市=====
+var jmf3=180000;//县级城市=====
+var jmVal; //加盟费
+//预算总投资
+function total(){
+	sDesign = 45000;//400平米以内设计费
+	area = getCookie("Area");//面积
+	city = getCookie("City");//成市
+	//rentVal =area * _rent * _rentT;//租金++++++++++++++++++++++++++++++++++++++++++
+    rentVal = Number($("#rent").val())*10000;	
+	zxVal = area * _zx;//装修费
+	cjVal = area*_cj;//厨具费=面积*每平米费用
+
+	/*加盟费*/
+	 $("#jmTxt").html(function () {
+		if(city.indexOf("北京")>=0 || city.indexOf("上海")>=0 || city.indexOf("广州")>=0 || city.indexOf("深圳")>=0 || city.indexOf("天津")>=0){
+			jmVal = jmf1;
+			return unit(jmVal) + "万";
+		}else if(city.indexOf("县")>=0){
+			jmVal = jmf3;
+			return unit(jmVal) + "万";
+		}else{    
+			jmVal = jmf2;
+			return unit(jmVal) + "万";
+		}
+	});
+	/*保证金*/
+	$("#bzTxt").html(unit(bzVal) + "万");
+	/*宣传费*/
+	$("#ggTxt").html(unit(ggVal) + "万");
+	/*营销系统、收银系统*/
+	$("#xtTxt").html(unit(xtVal) + "万");
+	/*面积*/
+	$("#areaTxt").html(area + "m<sup>2</sup>");
+	/*设置地区*/
+	$("#cityTxt").html(city);
+	/*前期人员招聘及培训*/
+	$("#pxTxt").html(unit(pxVal) + "万");
+	/*租金*/
+	$("#rentTxt").html(unit(rentVal) + "万");
+
+	/*装修费*/
+	$("#zxTxt").html(unit(zxVal) + "万");
+	/*设计费*/
+	$("#designTxt").html(function () {
+		if (area > 400) {
+			sDesign += (area - 400) * _design;
+		}
+		return unit(sDesign) + "万";
+	});
+	/*桌数*/
+	$("#zsTxt").html(function () {
+		cwVal = parseInt(area * _rj);//餐位=面积*容积率
+		zsVal = Math.ceil(cwVal / _ys);//桌数=餐位除以每桌椅数
+		return zsVal + "桌" + "(" + cwVal + ")个餐位";
+	});
+	/*餐桌/菜架/餐柜/餐椅*/
+	$("#czTxt").html(function () {
+		//桌单价*桌数+椅单价*餐位数
+		czVal = _zz * zsVal + _yz * cwVal;
+		return unit(czVal) + "万";
+	});
+	/*锅、锅圈、电磁炉*/
+	$("#gzTxt").html(function () {
+		gzVal = zsVal * _gz;
+		return unit(gzVal) + "万";
+	});
+	/*厨具*/
+	$("#cjTxt").html(unit(cjVal) + "万");
+	//餐具=面积*每平米费用
+	cajVal = area*_caj;
+	$("#cajTxt").html(unit(cajVal) + "万");
+	//杂件=面积*每平米费用
+	zjVal = area*_zaj;
+	$("#zajTxt").html(unit(zjVal) + "万");
+	/*调味品和菜品备货*/
+	$("#bhTxt").html(function () {
+		bhVal = _bh * zsVal;
+		return unit(bhVal) + "万";
+	});
+	$("#bcTxt").html(function () {
+		bcVal = _bc * zsVal;
+		return unit(bcVal) + "万";
+	});
+	/*员工宿舍费用*/
+	$(".ssTxt").html(function () {
+		if (area < 800) {
+			ssVal = _ss * 2;
+		} else if (area < 1000) {
+			ssVal = _ss * 3;
+		} else {
+			ssVal = _ss * 4;
+		}
+		return "<a>" + unit(ssVal) + "万/月</a>";
+	});
+	//总投资
+	$("#totalTxt").html(function () {
+		//alert(rentVal);
+		totalVal = rentVal + zxVal + sDesign + czVal + gzVal + cjVal + cajVal + zjVal + bhVal + bcVal + ssVal + pxVal + ggVal + bzVal + xtVal + jmVal;
+		if (isNaN(totalVal)) {
+			return 0;
+		} else {
+			return "<b>" + unit(totalVal) + "万</b>";
+		}
+	});
+	totalHB();//营业额，回报期
+}//预算总投资
+
+$("#sklTxt").change(function () {
+		totalHB();//营业额，回报期
+});
+
+//回报分析*********************************************
+//营业额，回报期
+function totalHB() {
+	//单价
+	var _ml = 0.58;//毛利率=====
+	var _xf = 80;//人均消费=====
+	var _sj = 0.035;//营业税
+	var _skl = $("#sklTxt").val();//上客率
+	if(_skl == 2){
+		var _sdq = 40;//每平米月水电费
+		var _zf = 40;//每平米月杂费
+		var _rg = 200;//每平米工资
+	}else if(_skl == 1.5){
+		var _sdq = 35;//每平米月水电费
+		var _zf = 35;//每平米月杂费
+		var _rg = 150;//每平米工资
+	}
+	//上客率与面积的关系
+	if(area >= 800){
+		_skl -= 0.35;
+	}else if(area >= 700){
+		_skl -= 0.25;
+	}else if(area >= 600){
+		_skl -= 0.15;
+	}else if(area >= 500){
+		_skl -= 0.1;
+	}else if(area >= 400){
+		_skl -= -0.03;
+	}else if(area >= 300){
+		_skl -= -0.06;
+	}else{
+		_skl -= -0.2;
+	}
+	//显示数据
+	$("#popleTxt").html(_ys + "位/桌");//桌人数
+	$("#mlTxt").html(Math.round(_ml * 100) + "%");//毛利
+	$("#xfTxt").html(_xf + "元/人");//人均消费
+	var rgVal;//员工工资
+	$("#rgTxt").html(function () {
+		rgVal = area * _rg;
+		return "<a>" + unit(rgVal) + "万/月</a>";
+	});
+	var MoonYY = cwVal * _xf * _skl * 30;//月营业额=餐位数*平均消费*上客率*30天
+	var MoonML = MoonYY * _ml; //月毛利=月营业额*毛利率
+	//水电气费
+	var sdqVal = area * _sdq;
+	$("#sdqTxt").html("<a>" + unit(sdqVal) + "万/月</a>");
+	//杂费
+	var zfVal = area * _zf;
+	$("#zfTxt").html("<a>" + unit(zfVal) + "万/月</a>");
+	//服务费
+    $("#fwTxt").html("<a>" + unit(fwVal) + "万/月</a>");
+	//税金
+	var sjVal = MoonYY * _sj;
+	$("#sjTxt").html("<a>" + unit(sjVal) + "万/月</a>");
+	//月开支=水电气+杂费+服务费+税金+工资+宿舍房租+//房租
+	var MoonKZ = sdqVal + zfVal + fwVal + sjVal+ rgVal + ssVal;
+	var MoonJL = MoonML - MoonKZ;
+	var YearJL = MoonJL*12;
+    $("#MoonYYTxt").html("<b>" + unit(MoonYY) + "万</b>");//月营业
+	$("#MoonMLTxt").html("<b>" + unit(MoonML) + "万</b>");//月毛利
+	$("#MoonJLTxt").html("<b>" + unit(MoonJL) + "万</b>");//月利润
+    $("#YearJLTxt").html("<b>" + unit(YearJL) + "万</b>");//年利润
+    $("#hbTxt").html("<b>" + Math.round(totalVal / (YearJL / 12)) + "个月</b>");
+}
+//转换为万
+function unit(a) {
+    return (a / 10000).toFixed(2);
+}
+
+
+/*内容数据*/
+$(".bzj").html(unit(bzVal) + "万");
+$(".jmf1").html(unit(jmf1) + "万");
+$(".jmf2").html(unit(jmf2) + "万");
+$(".jmf3").html(unit(jmf3) + "万");
